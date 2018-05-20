ce20e6f6d7e6dbe380333c8b734faa876ce508ec
DiNitride <j.ptb.t.ca@gmail.com>
2018-05-20 13:49:25 +0100

    Added Program

---
+++ CSGOMusicController/ControlWindow.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace CSGOMusicController
+{
+    partial class ControlWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.deadOrAlive = new System.Windows.Forms.Label();
+            this.stateLabel = new System.Windows.Forms.Label();
+            this.healthLabel = new System.Windows.Forms.Label();
+            this.healthIntLabel = new System.Windows.Forms.Label();
+            this.healthBar = new System.Windows.Forms.ProgressBar();
+            this.AudioDevicesList = new System.Windows.Forms.ComboBox();
+            this.StartStopToggle = new System.Windows.Forms.Button();
+            this.aliveVolumeInp = new System.Windows.Forms.NumericUpDown();
+            this.deadVolumeInp = new System.Windows.Forms.NumericUpDown();
+            this.AudioDeviceLabel = new System.Windows.Forms.Label();
+            this.AliveVolumeLabel = new System.Windows.Forms.Label();
+            this.DeadVolumeLabel = new System.Windows.Forms.Label();
+            this.Title = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.aliveVolumeInp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.deadVolumeInp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // deadOrAlive
+            // 
+            this.deadOrAlive.AutoSize = true;
+            this.deadOrAlive.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deadOrAlive.Location = new System.Drawing.Point(432, 50);
+            this.deadOrAlive.Name = "deadOrAlive";
+            this.deadOrAlive.Size = new System.Drawing.Size(42, 17);
+            this.deadOrAlive.TabIndex = 3;
+            this.deadOrAlive.Text = "Dead";
+            // 
+            // stateLabel
+            // 
+            this.stateLabel.AutoSize = true;
+            this.stateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stateLabel.Location = new System.Drawing.Point(381, 50);
+            this.stateLabel.Name = "stateLabel";
+            this.stateLabel.Size = new System.Drawing.Size(45, 17);
+            this.stateLabel.TabIndex = 4;
+            this.stateLabel.Text = "State:";
+            // 
+            // healthLabel
+            // 
+            this.healthLabel.AutoSize = true;
+            this.healthLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.healthLabel.Location = new System.Drawing.Point(272, 50);
+            this.healthLabel.Name = "healthLabel";
+            this.healthLabel.Size = new System.Drawing.Size(53, 17);
+            this.healthLabel.TabIndex = 5;
+            this.healthLabel.Text = "Health:";
+            // 
+            // healthIntLabel
+            // 
+            this.healthIntLabel.AutoSize = true;
+            this.healthIntLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.healthIntLabel.Location = new System.Drawing.Point(331, 50);
+            this.healthIntLabel.Name = "healthIntLabel";
+            this.healthIntLabel.Size = new System.Drawing.Size(16, 17);
+            this.healthIntLabel.TabIndex = 6;
+            this.healthIntLabel.Text = "0";
+            // 
+            // healthBar
+            // 
+            this.healthBar.Location = new System.Drawing.Point(275, 73);
+            this.healthBar.Name = "healthBar";
+            this.healthBar.Size = new System.Drawing.Size(252, 20);
+            this.healthBar.TabIndex = 7;
+            // 
+            // AudioDevicesList
+            // 
+            this.AudioDevicesList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.AudioDevicesList.FormattingEnabled = true;
+            this.AudioDevicesList.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.AudioDevicesList.Location = new System.Drawing.Point(129, 49);
+            this.AudioDevicesList.Name = "AudioDevicesList";
+            this.AudioDevicesList.Size = new System.Drawing.Size(123, 21);
+            this.AudioDevicesList.TabIndex = 8;
+            this.AudioDevicesList.SelectionChangeCommitted += new System.EventHandler(this.AudioDevicesList_SelectionChangeCommitted);
+            // 
+            // StartStopToggle
+            // 
+            this.StartStopToggle.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartStopToggle.Location = new System.Drawing.Point(204, 136);
+            this.StartStopToggle.Name = "StartStopToggle";
+            this.StartStopToggle.Size = new System.Drawing.Size(108, 44);
+            this.StartStopToggle.TabIndex = 9;
+            this.StartStopToggle.Text = "Start";
+            this.StartStopToggle.UseVisualStyleBackColor = true;
+            this.StartStopToggle.Click += new System.EventHandler(this.StartStopToggle_Click);
+            // 
+            // aliveVolumeInp
+            // 
+            this.aliveVolumeInp.Location = new System.Drawing.Point(129, 73);
+            this.aliveVolumeInp.Name = "aliveVolumeInp";
+            this.aliveVolumeInp.Size = new System.Drawing.Size(123, 20);
+            this.aliveVolumeInp.TabIndex = 10;
+            this.aliveVolumeInp.ValueChanged += new System.EventHandler(this.aliveVolumeInp_ValueChanged);
+            // 
+            // deadVolumeInp
+            // 
+            this.deadVolumeInp.Location = new System.Drawing.Point(129, 96);
+            this.deadVolumeInp.Name = "deadVolumeInp";
+            this.deadVolumeInp.Size = new System.Drawing.Size(123, 20);
+            this.deadVolumeInp.TabIndex = 11;
+            this.deadVolumeInp.ValueChanged += new System.EventHandler(this.deadVolumeInp_ValueChanged);
+            // 
+            // AudioDeviceLabel
+            // 
+            this.AudioDeviceLabel.AutoSize = true;
+            this.AudioDeviceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AudioDeviceLabel.Location = new System.Drawing.Point(12, 50);
+            this.AudioDeviceLabel.Name = "AudioDeviceLabel";
+            this.AudioDeviceLabel.Size = new System.Drawing.Size(88, 17);
+            this.AudioDeviceLabel.TabIndex = 12;
+            this.AudioDeviceLabel.Text = "Audio Player";
+            // 
+            // AliveVolumeLabel
+            // 
+            this.AliveVolumeLabel.AutoSize = true;
+            this.AliveVolumeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AliveVolumeLabel.Location = new System.Drawing.Point(12, 73);
+            this.AliveVolumeLabel.Name = "AliveVolumeLabel";
+            this.AliveVolumeLabel.Size = new System.Drawing.Size(89, 17);
+            this.AliveVolumeLabel.TabIndex = 13;
+            this.AliveVolumeLabel.Text = "Alive Volume";
+            // 
+            // DeadVolumeLabel
+            // 
+            this.DeadVolumeLabel.AutoSize = true;
+            this.DeadVolumeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeadVolumeLabel.Location = new System.Drawing.Point(12, 96);
+            this.DeadVolumeLabel.Name = "DeadVolumeLabel";
+            this.DeadVolumeLabel.Size = new System.Drawing.Size(93, 17);
+            this.DeadVolumeLabel.TabIndex = 14;
+            this.DeadVolumeLabel.Text = "Dead Volume";
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.Location = new System.Drawing.Point(150, 9);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(224, 25);
+            this.Title.TabIndex = 15;
+            this.Title.Text = "CS:GO Music Controller";
+            // 
+            // ControlWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 206);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.DeadVolumeLabel);
+            this.Controls.Add(this.AliveVolumeLabel);
+            this.Controls.Add(this.AudioDeviceLabel);
+            this.Controls.Add(this.deadVolumeInp);
+            this.Controls.Add(this.aliveVolumeInp);
+            this.Controls.Add(this.StartStopToggle);
+            this.Controls.Add(this.AudioDevicesList);
+            this.Controls.Add(this.healthBar);
+            this.Controls.Add(this.healthIntLabel);
+            this.Controls.Add(this.healthLabel);
+            this.Controls.Add(this.stateLabel);
+            this.Controls.Add(this.deadOrAlive);
+            this.MaximumSize = new System.Drawing.Size(555, 245);
+            this.MinimumSize = new System.Drawing.Size(555, 245);
+            this.Name = "ControlWindow";
+            this.Text = "CSGO Music Controller";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ControlWindow_FormClosing);
+            this.Load += new System.EventHandler(this.ControlWindow_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.aliveVolumeInp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.deadVolumeInp)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label deadOrAlive;
+        private System.Windows.Forms.Label stateLabel;
+        private System.Windows.Forms.Label healthLabel;
+        private System.Windows.Forms.Label healthIntLabel;
+        private System.Windows.Forms.ProgressBar healthBar;
+        private System.Windows.Forms.ComboBox AudioDevicesList;
+        private System.Windows.Forms.Button StartStopToggle;
+        private System.Windows.Forms.NumericUpDown aliveVolumeInp;
+        private System.Windows.Forms.NumericUpDown deadVolumeInp;
+        private System.Windows.Forms.Label AudioDeviceLabel;
+        private System.Windows.Forms.Label AliveVolumeLabel;
+        private System.Windows.Forms.Label DeadVolumeLabel;
+        private System.Windows.Forms.Label Title;
+    }
+}
+
---
+++ CSGOMusicController/ControlWindow.cs
@@ -0,0 +1,318 @@
+﻿using AudioSwitcher.AudioApi;
+using AudioSwitcher.AudioApi.CoreAudio;
+using AudioSwitcher.AudioApi.Session;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+
+namespace CSGOMusicController
+{
+    public partial class ControlWindow : Form
+    {
+
+        HttpListener postListener;
+        Thread loop;
+        IAudioSession player;
+        int health;
+        List<IAudioSession> sessions;
+        bool running = false;
+        double oldVolume, deadVol, aliveVol;
+
+        public ControlWindow()
+        {
+            InitializeComponent();
+            postListener = new HttpListener();
+            postListener.Prefixes.Add("http://127.0.0.1:5659/");
+            loop = new Thread(new ThreadStart(PollForData));
+            LoadFromFile();
+            
+        }
+
+        private void LoadFromFile()
+        {   
+            try
+            {
+                string readText = File.ReadAllText("automusic.config");
+                Console.WriteLine("Loaded Config: {0}", readText);
+                var vols = readText.Split(new string[] { "," }, StringSplitOptions.None);
+                Console.WriteLine("Dead Volume: {0} Alive Volume: {1}", vols[0], vols[1]);
+                int dVolT, aVolT;
+                if (Int32.TryParse(vols[0], out dVolT))
+                {
+                    Console.WriteLine("Success Converting Dead");
+                    if ((dVolT > 100) || (dVolT < 0))
+                    {
+                        Console.WriteLine("Loaded Dead Value OOB");
+                        dVolT = 50;
+                    }
+                    deadVol = dVolT;
+                }
+                if (Int32.TryParse(vols[1], out aVolT))
+                {
+                    Console.WriteLine("Success Converting Alive");
+                    if ((aVolT > 100) || (aVolT < 0))
+                    {
+                        Console.WriteLine("Loaded Alive Value OOB");
+                        aVolT = 50;
+                    }
+                    aliveVol = aVolT;
+                }
+                UpdateVolumeGui();
+            } catch (FileNotFoundException)
+            {
+                Console.WriteLine("Config File Not Found!");
+                defaultVolumes();
+            }
+        }
+
+        private void defaultVolumes()
+        {
+            deadVol = 100;
+            aliveVol = 50;
+            UpdateVolumeGui();
+        }
+
+        private void UpdateVolumeGui()
+        {
+            deadVolumeInp.Value = (int) deadVol;
+            aliveVolumeInp.Value = (int) aliveVol;
+        }
+
+        private void SaveFile()
+        {
+
+            string createText = deadVol + "," + aliveVol;
+            File.WriteAllText("automusic.config", createText);
+        }
+
+        public CoreAudioController Controller
+        {
+            get;  
+            private set;
+        }
+
+        private void ControlWindow_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("Starting Listen Server");
+            postListener.Start();
+            loop.Start();
+            CoreAudioController Controller = new CoreAudioController();
+            sessions = new List<IAudioSession>();
+            Console.WriteLine("Audio Sessions Discovered:");
+            foreach (var audioSession in Controller.DefaultPlaybackDevice.GetCapability<IAudioSessionController>())
+            {   
+                String name = audioSession.DisplayName;
+                if (!(name.Equals("")))
+                {
+                    AudioDevicesList.Items.Add(name);
+                    sessions.Add(audioSession);
+                    Console.WriteLine("--- {0}", name);
+                }
+            }
+        }
+
+        private void OnDeath()
+        {
+            Console.WriteLine("Player Dead!");
+            SetStateText("Dead");
+            SetHealth(health);
+            if (running)
+            {
+                Console.WriteLine("Set audio volume to {0}", deadVol);
+                player.SetVolumeAsync(deadVol);
+            }
+            
+        }
+
+        private void OnAlive()
+        {
+            Console.WriteLine("Player Alive!");
+            SetStateText("Alive!");
+            SetHealth(health);
+            if (running)
+            {
+                Console.WriteLine("Set audio volume to {0}", aliveVol);
+                player.SetVolumeAsync(aliveVol);
+            }
+            
+        }
+
+        private int GetHealth(HttpListenerRequest r)
+        {
+            System.IO.Stream body = r.InputStream;
+            System.Text.Encoding encoding = r.ContentEncoding;
+            System.IO.StreamReader reader = new System.IO.StreamReader(body, encoding);
+            string s = reader.ReadToEnd();
+            body.Close();
+            reader.Close();
+            DataJson d = JsonConvert.DeserializeObject<DataJson>(s);
+            try
+            {
+                return d.Player.State.Health;
+            } catch (NullReferenceException)
+            {
+                return 0;
+            }
+            
+        }
+
+        delegate void StringArgReturningVoidDelegate(string state);
+
+        delegate void IntArgReturningVoidDelegate(int health);
+
+        private void SetStateText(String state)
+        {
+            if (this.deadOrAlive.InvokeRequired)
+            {
+                StringArgReturningVoidDelegate d = new StringArgReturningVoidDelegate(SetStateText);
+                this.Invoke(d, new object[] { state });
+            }
+            else
+            {
+                this.deadOrAlive.Text = state;
+            }
+        }
+
+        private void SetHealth(int health)
+        {
+            if (this.healthIntLabel.InvokeRequired)
+            {
+                IntArgReturningVoidDelegate d = new IntArgReturningVoidDelegate(SetHealth);
+                this.Invoke(d, new object[] { health });
+            }
+            else
+            {
+                this.healthIntLabel.Text = health.ToString();
+                this.healthBar.Value = health;
+            }
+        }
+
+        private void PollForData()
+        {
+            HttpListenerContext context;
+            HttpListenerRequest request;
+            HttpListenerResponse response;
+            try
+            {
+                while (true)
+                {
+                    context = postListener.GetContext();
+                    request = context.Request;
+                    if (request.HttpMethod != "POST") { continue; }
+                    health = GetHealth(request);
+                    Console.WriteLine("Player Health: {0}", health);
+                    if (health > 0)
+                    {
+                        OnAlive();
+                    } else
+                    {
+                        OnDeath();
+                    }
+                    response = context.Response;
+                    response.Close();
+                }
+            }
+            catch (ThreadAbortException)
+            {
+                Console.WriteLine("Ended Loop Thread");
+            }
+        }
+
+        private void ControlWindow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveFile();
+            loop.Abort();
+            postListener.Stop();
+        }
+
+        private void AudioDevicesList_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            Console.WriteLine("Selected Audio Session '{0}' at index {1}", sessions[AudioDevicesList.SelectedIndex].DisplayName, AudioDevicesList.SelectedIndex);
+            player = sessions[AudioDevicesList.SelectedIndex];
+        }
+
+        private void StartStopToggle_Click(object sender, EventArgs e)
+        {
+            if (running == true)
+            {
+                // Program is changing volumes
+                Console.WriteLine("Stopping Program");
+                StartStopToggle.Text = "Start";
+                player.SetVolumeAsync(oldVolume);
+                AudioDevicesList.Enabled = true;
+                running = false;
+
+            } else
+            {
+                Console.WriteLine("Starting Program");
+                // Program is idle
+                if (player == null)
+                {
+                    return;
+                }
+                oldVolume = player.Volume;
+                StartStopToggle.Text = "Stop";
+                AudioDevicesList.Enabled = false;
+                running = true;
+            }
+        }
+
+        private void aliveVolumeInp_ValueChanged(object sender, EventArgs e)
+        {
+            aliveVol = (double) aliveVolumeInp.Value;
+            if (health > 0)
+            {
+                OnAlive();
+            }
+        }
+
+        private void deadVolumeInp_ValueChanged(object sender, EventArgs e)
+        {
+            deadVol = (double) deadVolumeInp.Value;
+            if (health == 0)
+            {
+                OnDeath();
+            }
+        }
+    }
+
+    public class DataJson
+    {
+        [JsonProperty("player")]
+        public PlayerJson Player { get; set; }
+
+        [JsonProperty("previously")]
+        public PreviouslyJson Previosuly { get; set; }
+    }
+
+    public class PlayerJson
+    {
+        [JsonProperty("state")]
+        public StateJson State { get; set; }
+    }
+
+    public class PreviouslyJson
+    {
+        [JsonProperty("previously")]
+        public PlayerJson Player { get; set; }
+    }
+
+    public class StateJson
+    {
+        [JsonProperty("health")]
+        public int Health { get; set; }
+    }
+
+}
